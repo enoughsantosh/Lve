8ef7b6e7811c94811d3ba9be2d8275cb3e803e2e
enoughsantosh <kalu4134@gmail.com>
2025-01-23 19:18:52 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,21 @@
 // Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
-import { getFirestore, doc, setDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+import { 
+    getFirestore, 
+    doc, 
+    setDoc, 
+    getDoc, 
+    updateDoc, 
+    serverTimestamp 
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
 // Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDLW0P7Op6coJkNzJj0ck9pLNdneJplcbI",
     authDomain: "love-6996.firebaseapp.com",
     projectId: "love-6996",
-    storageBucket: "love-6996.firebasestorage.app",
+    storageBucket: "love-6996.appspot.com",
     messagingSenderId: "1050433920317",
     appId: "1:1050433920317:web:99246747017f6bdca1b954",
     measurementId: "G-EKDTE6WQY1"
@@ -21,4 +28,15 @@ const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
 
 // Export Firebase modules
-export { auth, db, googleProvider, signInWithPopup, doc, setDoc, serverTimestamp };
+export { 
+    app, 
+    auth, 
+    db, 
+    googleProvider, 
+    signInWithPopup, 
+    doc, 
+    setDoc, 
+    getDoc, 
+    updateDoc, 
+    serverTimestamp 
+};
